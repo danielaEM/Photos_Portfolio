562c42daa1ce624351e96f7acdb58abc38c31fb2
danielaem <dana.mariuta@yahoo.com>
2020-05-23 14:01:58 +0300

    Add Read More button and functionality, replace mock text

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("moreText");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
